9888dc7e5f6030d98c21f7795de338af0ba30a08
Dan Lesky <dan.lesky@gmail.com>
2022-11-08 13:23:44 -0700

    Add variables and console commands

--- variables.js
+++ variables.js
@@ -6,8 +6,16 @@ var theYear = 2022
 var luckyNumber = 7
 var forSure = 100
 
-var jsIsFun = True
-var jsIsPopular = True
-var jsIsBoring = False 
+var jsIsFun = true
+var jsIsPopular = true
+var jsIsBoring = false 
+
+theLanguage = "Spanish"
+bestPuppy = "Shimmy"
+bestCat = "Peaks"
+
+console.log("The basement is chilly")
+console.log("It's sunny outside")
+console.log(`Can't wait to see ${bestPuppy}`)
 
 
